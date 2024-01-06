e2da435d73d967928e5df4834f474eff65604521
GirOval <151110458+GirOval@users.noreply.github.com>
2024-01-06 17:07:21 +0700

    Add files via upload

--- HitJauhPB.c
+++ HitJauhPB.c
@@ -6,14 +6,16 @@ double v0X(double v0, double sudut), v0Y(double v0, double sudut);
 double radian(double sudut);
 double vX(double v0x), vY(double v0y, double g, double t);
 double X(double vx, double t), Y(double vy, double t, double g);
+double Xmax(double v0, double g, double rad), Ymax(double v0, double g, double rad);
+double Tx(double v0, double g, double rad);
 
 int main() 
 {
     char cari, subCari;
-    double v0, vx, vy, v0x, v0y, x, y, vt, t, h, g, sudut, rad;
+    double v0, vx, vy, v0x, v0y, x, y, xmax, ymax, thmax, txmax, vt, t, g, sudut, rad;
 
     do{
-        printf("Ini adalah program penyelesaian yang berhubungan dengan\ngerak parabola. Apa yang mau di cari ?\n A.Kecepatan awal horizontal dan vertikal\nV_xy = v_0 * cos-sin dari(sudut-radian)\n\n B.Kecepatan horizontal(opsional) dan vertikal\n V_x = V0x\nV_y = V_0y - g * t\n\n C.Jarak horizontal dan vertikal\nX = Vx * t\ny = Vx * t - g * pow(t)\n\n G.Jarak terjauh\n H.Ketinggian maksimum\n I.Kecepatan total benda\n");
+        printf("Ini adalah program penyelesaian yang berhubungan dengan\ngerak parabola. Apa yang mau di cari ?\n A.Kecepatan awal horizontal dan vertikal\nV_xy = v_0 * cos-sin dari(sudut-radian)\n\n B.Kecepatan horizontal(opsional) dan vertikal\nV_x = V0x\nV_y = V_0y - g * t\n\n C.Jarak horizontal dan vertikal\nX = Vx * t\ny = Vx * t - g * pow(t)\n\n D.Jarak terjauh dan Ketinggian maksimum\nXmax = Pow(V0) * (sin * 2sudut) / g\nYmax = Pow(V0) * Pow(sin(sudut))\n\n E.Waktu untuk mencapai titik terjauh dan tertinggi\ntxmax = 2 * v0 * sin(sudut) / g\nthmax = v0 * sin(sudut) / g\n\n I.Kecepatan total benda\n");
         scanf("%c", &cari);
     }while (cari == '\n');
    
@@ -127,8 +129,42 @@ int main()
             printf("%c tidak valid, lebih teliti lagi!", subCari);
             break;
         }
+        break;
+
+    case 'D':
+            printf(" Diketahui\n Kecepatan awal:");
+            scanf("%lf", &v0);
+            printf(" Sudut:");
+            scanf("%lf", &sudut);
+            printf(" Gravitasi:");
+            scanf("%lf", &g);
+
+            rad = radian(sudut);
+            xmax = Xmax(v0, g, rad);
+            ymax = Ymax(v0, g, rad);
+
+            printf("Xmax = Pow(%lf) * (sin(%lf) * 2) / %lf = %lf\n", v0, sudut, g, xmax);
+            printf("Ymax = Pow(%lf) * Pow(sin(%lf)) / %lf = %lf", v0, sudut, g, ymax);
+
             break;
 
+        case 'E':
+            printf(" Diketahui\n Kecepatan awal:");
+            scanf("%lf", &v0);
+            printf(" Sudut:");
+            scanf("%lf", &sudut);
+            printf(" Gravitasi:");
+            scanf("%lf", &g);
+            
+            rad = radian(sudut);
+            txmax = 2 * Tx(v0, g, rad);
+            thmax = Tx(v0, g, rad);
+
+            printf("txmax = 2 * %lf * cos(%lf) / %lf = %lf\n", v0, sudut, g, txmax);
+            printf("thmax = %lf * cos(%lf) / %lf = %lf", v0, sudut, g, thmax);
+            
+            break;
+            
     default:
         printf("Input dengan opsi yang tersedia menggunakan huruf besar");
         break;
@@ -154,4 +190,13 @@ double X(double vx, double t){
 } 
 double Y(double vy, double t, double g){
     return (vy * t) - (0.5 * g * pow(t, 2));
+}
+double Xmax(double v0, double g, double rad){
+    return pow(v0, 2) * (sin(rad) * 2) / g;
+}
+double Ymax(double v0, double g, double rad){
+    return pow(v0, 2) * pow(sin(rad), 2) / g;
+}
+double Tx(double v0, double g, double rad){
+    return v0 * sin(rad) / g;
 }
